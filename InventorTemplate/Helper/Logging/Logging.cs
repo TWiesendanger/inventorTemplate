ebda206eb91374125bb106e646c15ab27561c9fb
Tobias Wiesendanger <tobias.wiesendanger@mum.ch>
2022-08-08 19:31:54 +0200

    added loggin poco

---
+++ InventorTemplate/Helper/Logging/Logging.cs
@@ -0,0 +1,12 @@
+﻿namespace InventorTemplate.Helper.Logging
+{
+    public class Logging
+    {
+	    public string LogPath { get; set; }
+    }
+
+    public class AppSettings
+    {
+	    public string Setting1 { get; set; }  
+    }
+}
