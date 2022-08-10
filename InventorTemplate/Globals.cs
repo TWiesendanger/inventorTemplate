97f73335f9f6ec996639f02cb54ab1a81a34764d
Tobias Wiesendanger <tobias.wiesendanger@gmail.com>
2022-08-10 23:51:29 +0200

    added ApplicationAddInSite

--- InventorTemplate/Globals.cs
+++ InventorTemplate/Globals.cs
@@ -3,6 +3,7 @@
     public static class Globals
     {
         public static Inventor.Application InvApp;
+        public static Inventor.ApplicationAddInSite InvApplicationAddInSite;
         public const string AddInClientId = "74F1F066-BB65-4141-BDC8-C4A7669913F1";
     }
 }
