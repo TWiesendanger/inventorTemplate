bc5cf0add6ab5f1d72cfb5a12a2ab56ab0434ffc
Tobias Wiesendanger <tobias.wiesendanger@gmail.com>
2022-08-12 01:35:51 +0200

    removed solution parameter (no longer needed)

--- TemplateHelpers/SampleUse.ps1
+++ TemplateHelpers/SampleUse.ps1
@@ -1,2 +1,2 @@
-﻿dotnet new invAddin -n Testaddin -o "C:\temp\sampleAddin\Testaddin" -solFld "C:\temp\sampleAddin" -instFld "C:\ProgramData\Company\sampleAddin"
+﻿dotnet new invAddin -n Testaddin -o "C:\temp\sampleAddin\" -instFld "C:\ProgramData\Company\sampleAddin"
 Read-Host
